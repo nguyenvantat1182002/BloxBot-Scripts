270a829a2710fe82d93b28231f552d87a0129315
Nguyễn Văn Tất <69678377+nguyenvantat1182002@users.noreply.github.com>
2024-08-19 18:29:22 +0700

    Add files via upload

---
+++ getpets.lua
@@ -0,0 +1,32 @@
+repeat task.wait(1) until game:IsLoaded()
+
+local Players = game:GetService'Players'
+local LocalPlayer = Players.LocalPlayer
+if not LocalPlayer then
+    repeat
+        LocalPlayer = Players.LocalPlayer
+        task.wait(1)
+    until LocalPlayer
+end
+task.wait(1)
+
+local HttpService = game:GetService('HttpService')
+local Library = require(game.ReplicatedStorage.Library.Client.Save).Get()
+
+
+function writeData(request, text)
+    local data = HttpService:JSONEncode {
+        Request = request,
+        Text = text
+    }
+    writefile(LocalPlayer.Name .. '.json', data)
+end
+
+
+local pets = {}
+for _, v in pairs(Library.Inventory.Pet) do
+    table.insert(pets, v.id)
+end
+
+local petsString = table.concat(pets, ",")
+writeData('Completed', 'Pets: ' .. petsString)
