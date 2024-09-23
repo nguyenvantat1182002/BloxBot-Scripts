ff1d3efc91c99d7f542b5cd159c4fa356b8370b3
Nguyễn Văn Tất <69678377+nguyenvantat1182002@users.noreply.github.com>
2024-09-23 23:48:16 +0700

    Add files via upload

---
+++ av_track.lua
@@ -0,0 +1,52 @@
+repeat wait() until game:IsLoaded()
+
+local HttpService = game:GetService'HttpService'
+local Players = game:GetService'Players'
+local LocalPlayer = Players.LocalPlayer
+if not LocalPlayer then
+    repeat
+        LocalPlayer = Players.LocalPlayer
+        task.wait(1)
+    until LocalPlayer
+end
+task.wait(1)
+
+
+function getLevel()
+    local pattern = "Level (%d+)"
+    local s = ''
+
+    if game.placeId == 16146832113 then
+        s = LocalPlayer.PlayerGui.HUD.Main.Level.Level.Text
+    else
+        s = LocalPlayer.PlayerGui.Hotbar.Main.Level.Level.Text
+    end
+
+    local level = string.match(s, pattern)
+    return tonumber(level)
+end
+
+function WriteData(request, text)
+    local data = HttpService:JSONEncode {
+        Request = request,
+        Text = text
+    }
+    writefile(LocalPlayer.Name .. '.json', data)
+end
+
+local updateInterval = 30
+local maximumLevel = 30
+
+while true do
+    local level = getLevel()
+    local s = 'Level: '..tostring(level)
+
+    if level >= maximumLevel then
+        WriteData('Completed', s)
+        return
+    end
+    
+    WriteData('', s)
+
+    task.wait(updateInterval)
+end
