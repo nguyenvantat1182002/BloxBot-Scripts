d662849c739512cfdf35b78921460653e564da12
lytrihangit <63119929+lytrihangit@users.noreply.github.com>
2025-06-29 12:37:13 +0700

    Update gag.lua

--- gag.lua
+++ gag.lua
@@ -145,7 +145,8 @@ local function buyEggs()
                 for i = 1, 3 do 
                     BuyPetEgg:FireServer(i)
                 end
-                done = true
+                
+		break
             end
         end
         task.wait(5)
